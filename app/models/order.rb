7b26a09e024b27133c41782bb55718127cd91b65
Olena <helen8here@gmail.com>
2017-06-21 00:10:33 +0300

    valitations + admin + something else

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,12 @@
 class Order < ActiveRecord::Base
-  validates_datetime :datetime_of_purchase
+  validates :datetime_of_purchase, timeliness: {type: :datetime}
+
+
   # validates :city_id, presence: true
   # validates :customer_id, presence: true
   # validates :supplier_id, presence: true
 
   belongs_to :customer
   belongs_to :supplier
-  belongs_to :order
+  belongs_to :city
 end
