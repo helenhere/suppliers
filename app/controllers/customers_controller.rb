df89970d36eebe7eed21ef9f64173fd19d786468
Olena <helen8here@gmail.com>
2017-06-23 12:52:10 +0300

    fixed DRY

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -57,59 +57,6 @@ class CustomersController < ApplicationController
       redirect_to customers_path
   end
 
-  def logged_in_customer
-    unless logged_in?
-      redirect_to login_url
-    end
-  end
-
-  def correct_customer
-    @customer = Customer.find(params[:id])
-    redirect_to(orders_path) unless @customer.current_customer?(@customer)
-  end
-
-  def log_in(customer)
-    session[:customer_id] = customer.id
-  end
-
-  def logged_in?
-    !@current_customer.nil?
-  end
-
-  def current_customer
-    if (customer_id = session[:customer_id])
-      @current_customer ||= Customer.find_by(id: customer_id)
-    elsif (customer_id = cookies.signed[:customer_id])
-      customer = Customer.find_by(id: customer_id)
-      if customer && customer.authenticated?(cookies[:remember_token])
-        log_in customer
-        @current_customer = customer
-      end
-    end
-  end
-
-  def remember(customer)
-    customer.remember
-    cookies.permanent.signed[:customer_id] = customer.id
-    cookies.permanent[:remember_token] = customer.remember_token
-  end
-
-  def current_customer?(customer)
-    customer == current_customer
-  end
-
-  def forget(customer)
-    customer.forget
-    cookies.delete(:customer_id)
-    cookies.delete(:remember_token)
-  end
-
-  def log_out
-    forget(@current_customer)
-    session.delete(:customer_id)
-    @current_customer = nil
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_customer
@@ -119,6 +66,17 @@ class CustomersController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def customer_params
       params.require(:customer).permit(:first_name, :last_name, :phone, :email, :password, :city_id)
+    end
+
+    def logged_in_customer
+      unless logged_in?
+        redirect_to login_url
+      end
+    end
+
+    def correct_customer
+      @customer = Customer.find(params[:id])
+      redirect_to(orders_path) unless @customer.current_customer?(@customer)
     end
 end
 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,30 +20,4 @@ class SessionsController < ApplicationController
     log_out if logged_in?
     redirect_to root_path
   end
-
-  def log_in(customer)
-    session[:customer_id] = customer.id
-  end
-
-  def logged_in?
-    !@current_customer.nil?
-  end
-
-  def log_out
-    forget(@current_customer)
-    session.delete(:customer_id)
-    @current_customer = nil
-  end
-
-  def remember(customer)
-    customer.remember
-    cookies.permanent.signed[:customer_id] = customer.id
-    cookies.permanent[:remember_token] = customer.remember_token
-  end
-
-  def forget(customer)
-    customer.forget
-    cookies.delete(:customer_id)
-    cookies.delete(:remember_token)
-  end
 end
